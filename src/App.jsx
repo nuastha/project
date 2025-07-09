41ff3354eed03bcd97d0e7a586d729acf66867df
nuastha <timilhllo@gmail.com>
2025-07-09 23:20:16 +0545

    Implement routing with React Router for Home, Contact, About, and Our Teams pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,18 @@
+import { Route, Routes } from "react-router-dom";
+import HomePage from "./page/homepage/HomePage";
+import ContactUs from "./page/contact-us/ContactUs";
+import AboutUs from "./page/about-us/AboutUs";
+import OurTeams from "./page/our-teams/OurTeams";
+
 function App() {
-  return <div>hello world</div>;
+  return (
+    <Routes>
+      <Route index element={<HomePage />} />
+      <Route path="/contact-us" element={<ContactUs />} />
+      <Route path="/about-us" element={<AboutUs />} />
+      <Route path="/our-teams" element={<OurTeams />} />
+    </Routes>
+  );
 }
 
 export default App;
