881f4baf2926b6c4676236d0dccf18b1bab37ff0
nuastha <timilhllo@gmail.com>
2025-07-10 12:13:16 +0545

    Add components to render AboutUs, OurTeams, and ContactUs sections on HomePage

--- src/page/homepage/HomePage.jsx
+++ src/page/homepage/HomePage.jsx
@@ -1,8 +1,14 @@
+import AboutUs from "../../section/about-us/AboutUs";
+import OurTeams from "../../section/our-teams/OurTeams";
 import Slider from "../../section/slider/Slider";
+import ContactUs from "../../section/contact-us/ContactUs";
 export default function App() {
   return (
     <div className="">
       <Slider />
+      <AboutUs />
+      <OurTeams />
+      <ContactUs />
     </div>
   );
 }
