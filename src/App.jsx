ff3d5597e3b670599ec4c4370641e29d52fec056
nuastha <timilhllo@gmail.com>
2025-07-11 12:23:05 +0545

    `Removed unused routes and API calls, updated text alignment in ContactUs component`

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import HomePage from "./page/homepage/HomePage";
 import ContactUs from "./page/contact-us/ContactUs";
 import AboutUs from "./page/about-us/AboutUs";
 import OurTeams from "./page/our-teams/OurTeams";
+import Products from "./page/fetching/Products";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Route path="/contact-us" element={<ContactUs />} />
       <Route path="/about-us" element={<AboutUs />} />
       <Route path="/our-teams" element={<OurTeams />} />
+      <Route path="/products" element={<Products />} />
     </Routes>
   );
 }
